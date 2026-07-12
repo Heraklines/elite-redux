6753c01f894911c0e54efd9c1c0babab6b3bced1
heraklines <heraklines@tutanota.com>
2026-07-12 12:24:33 +0200

    test(coop): type bargain UI callbacks

--- test/tools/coop-soak-driver.ts
+++ test/tools/coop-soak-driver.ts
@@ -1755,9 +1755,11 @@ export async function runCoopSoak(game: GameManager, opts: SoakOptions): Promise
           };
           ui.showDialogue = (...args: unknown[]): unknown => {
             (args[3] as (() => void) | undefined)?.();
+            return undefined;
           };
           ui.showText = (...args: unknown[]): unknown => {
             (args[2] as (() => void) | undefined)?.();
+            return undefined;
           };
           ownerPhase.start();
           for (let i = 0; i < 24 && !ended; i++) {
